1e6847ebd53d89c82b7b6d00cf54197545a4da65
Fergus Farrell <fergusfrl@gmail.com>
2017-06-25 13:14:27 +1200

    Add files via upload
    Added external javascript file to increase readability

---
+++ Development/myScripts.js
@@ -0,0 +1,39 @@
+var greenMarker = 'http://maps.google.com/mapfiles/ms/icons/green.png'
+var redMarker = 'http://maps.google.com/mapfiles/ms/icons/red.png'
+var orangeMarker = 'http://maps.google.com/mapfiles/ms/icons/orange.png'
+var rivers = [
+	["Upper Taieri", -45.649560, 170.276538, 1, greenMarker],
+	["Lower Taieri", -45.792323, 170.309228, 2, greenMarker],
+	["Leith", -45.864321, 170.514365, 3, redMarker],
+	["Tekapo Whitewater Course", -44.020199, 170.468593, 4, greenMarker]
+]
+
+//Define Map Properties
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 6,
+		center: {lat: -40.834541, lng: 173.473535}
+	});
+	setMarkers(map);
+}
+		
+//Add Interactive River Markers to Map
+function setMarkers(map){
+	
+	var shape = {
+		coords: [1, 1, 1, 20, 18, 20, 18, 1],
+		type: 'poly'
+	};
+			
+	for(var i = 0; i < rivers.length; i++){
+		var river = rivers[i];
+		var marker  = new google.maps.Marker({	
+			position: {lat: river[1], lng: river[2]},
+			map: map,
+			icon: river[4],
+			shape: shape,
+			title: river[0],
+			zIndex: river[3]
+		});
+	}
+}
