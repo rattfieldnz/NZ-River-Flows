c2fc600a7468eaad3b07988466ed0e2c5f2cb160
Fergus Farrell <fergusfrl@gmail.com>
2017-07-06 19:08:37 +1200

    Add files via upload
    Search with zoom is working!!

--- resources/js/eventListeners.js
+++ resources/js/eventListeners.js
@@ -1,3 +1,33 @@
 document.getElementById('searchButton').addEventListener('click', function(){
-	console.log("Working!");
+	zoomOnSearch();
 });
+
+document.getElementById('item').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      zoomOnSearch();
+    }
+});
+
+function zoomOnSearch(){
+	var searchValue = document.getElementById('item').value;
+	if(searchValue) {
+		var numberReturned = 0;
+		var lt = 0;
+		var ln = 0;
+		for(var i = 0; i < rivers.length; i++){
+			if(rivers[i][0].toLowerCase().indexOf(searchValue.toLowerCase()) !== -1){
+				lt = rivers[i][1];
+				ln = rivers[i][2];
+				numberReturned++;
+			}
+		}
+		if(numberReturned == 1){
+			var pos = {
+				lat: lt,
+				lng: ln
+			};
+			zoom(pos, true);
+		}
+	}
+};
