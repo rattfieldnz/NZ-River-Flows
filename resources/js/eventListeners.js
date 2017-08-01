bfdc1088a635b5716836239fd614883c2cad9801
Fergus Farrell <fergusfrl@gmail.com>
2017-08-01 19:02:27 +1200

    Add files via upload
    Filters and search bar working :)
    Somethings wring with info windows :0

--- resources/js/eventListeners.js
+++ resources/js/eventListeners.js
@@ -1,6 +1,8 @@
 
 var linkSVG = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;" xml:space="preserve"><g><g><path d="M392.857,292.354h-18.274c-2.669,0-4.859,0.855-6.563,2.573c-1.718,1.708-2.573,3.897-2.573,6.563v91.361 c0,12.563-4.47,23.315-13.415,32.262c-8.945,8.945-19.701,13.414-32.264,13.414H82.224c-12.562,0-23.317-4.469-32.264-13.414 c-8.945-8.946-13.417-19.698-13.417-32.262V155.31c0-12.562,4.471-23.313,13.417-32.259c8.947-8.947,19.702-13.418,32.264-13.418 h200.994c2.669,0,4.859-0.859,6.57-2.57c1.711-1.713,2.566-3.9,2.566-6.567V82.221c0-2.662-0.855-4.853-2.566-6.563 c-1.711-1.713-3.901-2.568-6.57-2.568H82.224c-22.648,0-42.016,8.042-58.102,24.125C8.042,113.297,0,132.665,0,155.313v237.542 c0,22.647,8.042,42.018,24.123,58.095c16.086,16.084,35.454,24.13,58.102,24.13h237.543c22.647,0,42.017-8.046,58.101-24.13 c16.085-16.077,24.127-35.447,24.127-58.095v-91.358c0-2.669-0.856-4.859-2.574-6.57 C397.709,293.209,395.519,292.354,392.857,292.354z"/><path d="M506.199,41.971c-3.617-3.617-7.905-5.424-12.85-5.424H347.171c-4.948,0-9.233,1.807-12.847,5.424 c-3.617,3.615-5.428,7.898-5.428,12.847s1.811,9.233,5.428,12.85l50.247,50.248L198.424,304.067 c-1.906,1.903-2.856,4.093-2.856,6.563c0,2.479,0.953,4.668,2.856,6.571l32.548,32.544c1.903,1.903,4.093,2.852,6.567,2.852 s4.665-0.948,6.567-2.852l186.148-186.148l50.251,50.248c3.614,3.617,7.898,5.426,12.847,5.426s9.233-1.809,12.851-5.426 c3.617-3.616,5.424-7.898,5.424-12.847V54.818C511.626,49.866,509.813,45.586,506.199,41.971z"/></g></g></svg>'
 var starSVG = '<svg version="1.1" id="Capa_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 49.94 49.94" style="enable-background:new 0 0 49.94 49.94;" xml:space="preserve"><path d="M48.856,22.731c0.983-0.958,1.33-2.364,0.906-3.671c-0.425-1.307-1.532-2.24-2.892-2.438l-12.092-1.757 c-0.515-0.075-0.96-0.398-1.19-0.865L28.182,3.043c-0.607-1.231-1.839-1.996-3.212-1.996c-1.372,0-2.604,0.765-3.211,1.996 L16.352,14c-0.23,0.467-0.676,0.79-1.191,0.865L3.069,16.623C1.71,16.82,0.603,17.753,0.178,19.06 c-0.424,1.307-0.077,2.713,0.906,3.671l8.749,8.528c0.373,0.364,0.544,0.888,0.456,1.4L8.224,44.702 c-0.232,1.353,0.313,2.694,1.424,3.502c1.11,0.809,2.555,0.914,3.772,0.273l10.814-5.686c0.461-0.242,1.011-0.242,1.472,0 l10.815,5.686c0.528,0.278,1.1,0.415,1.669,0.415c0.739,0,1.475-0.231,2.103-0.688c1.111-0.808,1.656-2.149,1.424-3.502 L39.651,32.66c-0.088-0.513,0.083-1.036,0.456-1.4L48.856,22.731z M37.681,32.998l2.065,12.042c0.104,0.606-0.131,1.185-0.629,1.547 c-0.499,0.361-1.12,0.405-1.665,0.121l-10.815-5.687c-0.521-0.273-1.095-0.411-1.667-0.411s-1.145,0.138-1.667,0.412l-10.813,5.686 c-0.547,0.284-1.168,0.24-1.666-0.121c-0.498-0.362-0.732-0.94-0.629-1.547l2.065-12.042c0.199-1.162-0.186-2.348-1.03 3.17 L2.48,21.299c-0.441-0.43-0.591-1.036-0.4-1.621c0.19-0.586,0.667-0.988,1.276-1.077l12.091-1.757 c1.167-0.169,2.176-0.901,2.697-1.959l5.407-10.957c0.272-0.552,0.803-0.881,1.418-0.881c0.616,0,1.146,0.329,1.419,0.881 l5.407,10.957c0.521,1.058,1.529,1.79,2.696,1.959l12.092,1.757c0.609,0.089,1.086,0.491,1.276,1.077 c0.19,0.585,0.041,1.191-0.4,1.621l-8.749,8.528C37.866,30.65,37.481,31.835,37.681,32.998z"/><g></g></svg>';
+var checkArray = [1, 2, 3, 4, 5];
+var globalSearchValue = "";
 
 renderRiverList();
 
@@ -30,9 +32,10 @@ document.getElementById('flowIcon').addEventListener('click', function(){
 });
 
 checkBoxes();
-
+var flag = true;
 // Prints current checkbox values
 function checkBoxes(){
+	
 	var boxes = document.getElementsByClassName('gradeBox');
 	for (var i = 0; i < boxes.length; i++){
 		boxes[i].addEventListener('click', function(){
@@ -41,46 +44,31 @@ function checkBoxes(){
 	}
 }
 
-var checkArray = [];
 function printBoxes(boxes){
+	if(flag){
+		checkArray = [];
+		flag = !flag;
+	}
 	for (var i = 0; i < boxes.length; i++){
 		if(boxes[i].checked){
-			checkArray.push(boxes[i].value);
+			checkArray.push(parseInt(boxes[i].value));
 		}
 	}
 	clearList();
 	
-	for (var i = 0; i < rivers.length; i++){
-		var river = rivers[i];
-		if (checkArray.indexOf(river.grade.toString()) >= 0){
-			var pos = {
-				lat: river.lat,
-				lng: river.lng
-			};
-			addItemToList(river.title, river.grade, pos, river.url);
-		}
-	}
+	addItemToList();
+	
 	checkArray = [];
 }
 
-var searchValueX = '';
 // Re-renders list depending on search value
 function searchList(field) {
+	globalSearchValue = field.value.toLowerCase();;
 	clearList();
-	searchValueX = field.value.toLowerCase();
-	
-	for(var i = 0; i < rivers.length; i++){
-		var searchValue = field.value.toLowerCase();
-		var river = rivers[i];
-		if((river.title.toLowerCase().indexOf(searchValue) != -1) || (river.river.toLowerCase().indexOf(searchValue) != -1)){
-			var pos = {
-				lat: river.lat,
-				lng: river.lng
-			}
-			addItemToList(river.title, river.grade, pos, river.url);
-		}
-	}
+	var boxes = document.getElementsByClassName('gradeBox');
+	printBoxes(boxes);
 }
+
 
 // Map zooms into coordinates of search value
 function zoomOnSearch(){
@@ -109,69 +97,62 @@ function zoomOnSearch(){
 
 // Renders river list
 function renderRiverList() {
-	for(var i = 0; i < rivers.length; i++){
-		var river = rivers[i];
-		var linkTo = river.url;
-		var pos = {
-			lat: river.lat,
-			lng: river.lng
-		};
-		addItemToList(river.title, river.grade, pos, linkTo);
-	}
+	addItemToList();
 }
 
 // Adds items to list - includes event listeners for li items
-function addItemToList(river, grade, pos, linkTo){
+function addItemToList(){
+
+	for(var i = 0; i < rivers.length; i++){
+		var river = rivers[i];
+		if((checkArray.indexOf(river.grade) >= 0) && (river.title.toLowerCase().indexOf(globalSearchValue) >= 0)){
+			console.log(river.title);
+			
+			var list = document.getElementById('riverList');
 	
-	var riverX = '';
-	for (var i = 0; i < rivers.length; i++){
-		riverX = rivers[i];
-		if((checkArray.indexOf(riverX.grade.toString()) >= 0) 
-			&& ((riverX.title.toLowerCase().indexOf(searchValueX) != -1) 
-			|| (riverX.river.toLowerCase().indexOf(searchValueX) != -1))){
-			console.log(riverX.title);
+			var item = document.createElement('li');
+			item.innerText = river.title;
+			
+			var buttons = document.createElement('div');
+			buttons.classList.add('buttons');
+			
+			//var starIcon = document.createElement('button');
+			//starIcon.classList.add('starIcon');
+			//starIcon.innerHTML = starSVG;
+			
+			var linkIcon = document.createElement('button');
+			linkIcon.classList.add('linkIcon');
+			linkIcon.innerHTML = linkSVG;
+			
+			buttons.appendChild(linkIcon);
+			//buttons.appendChild(starIcon);
+			
+			var content = "<h4>" + river.title + "</h4>";
+				content += "<p>Grade " + river.grade + "</p>";
+				
+			var pos = {
+				lat: river.lat,
+				lng: river.lng
+			};
+			
+			item.addEventListener('click', function(){
+				openInfoWindow(content, pos, true);
+				zoom(pos, true);
+			});
+			
+			item.addEventListener('mouseover', function(){
+				openInfoWindow(content, pos, false);
+			});
+			
+			linkIcon.addEventListener('click', function(){
+				window.location.href = river.url;
+			});
+			
+			
+			item.appendChild(buttons);
+			list.appendChild(item);
 		}
 	}
-	
-	var list = document.getElementById('riverList');
-	
-	var item = document.createElement('li');
-	item.innerText = river;
-	
-	var buttons = document.createElement('div');
-	buttons.classList.add('buttons');
-	
-	//var starIcon = document.createElement('button');
-	//starIcon.classList.add('starIcon');
-	//starIcon.innerHTML = starSVG;
-	
-	var linkIcon = document.createElement('button');
-	linkIcon.classList.add('linkIcon');
-	linkIcon.innerHTML = linkSVG;
-	
-	buttons.appendChild(linkIcon);
-	//buttons.appendChild(starIcon);
-	
-	var content = "<h4>" + river + "</h4>";
-		content += "<p>Grade " + grade + "</p>";
-	
-	item.addEventListener('click', function(){
-		openInfoWindow(content, pos, true);
-		zoom(pos, true);
-	});
-	
-	item.addEventListener('mouseover', function(){
-		openInfoWindow(content, pos, false);
-	});
-	
-	linkIcon.addEventListener('click', function(){
-		window.location.href = linkTo;
-	});
-	
-	
-	
-	item.appendChild(buttons);
-	list.appendChild(item);
 }
 
 // Opens an infor window at coordinates
@@ -201,7 +182,8 @@ function clearList(){
 }
 
 //jQuery begins here
-$(document).ready(function() {
+
+/*$(document).ready(function() {
 	
    // Loads from external URL - NOT WORKING
    var url = 'http://data.orc.govt.nz/v1/sql/TEL_RIVF/?$filter=Site%20eq%20%27Taieri%20At%20Outram%27&format=json';
@@ -227,4 +209,4 @@ $(document).ready(function() {
 	  }
    });
    
-});
+});*/
